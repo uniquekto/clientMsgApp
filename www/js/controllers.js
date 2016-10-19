0ab22fd50cd5b8b3649a9288a3204996d4b37f7b
Pramesh <pramesh_morang@hotmail.com>
2016-10-20 01:01:01 +0300

    removed some unnessary code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,20 +16,19 @@ angular.module('starter.controllers', [])
  $scope.user= {
      message: ""
  };
-    console.log($stateParams);
-    
+    console.log($stateParams);  
     $http.get("https://messageapp1.herokuapp.com/messages").then(function(response){
         console.log(response);
         $scope.data=response.data;
 });
     
 $scope.send = function()
-  {
-    console.log($scope.user.message);
+  {  
     var dataWithPost={
       username:$stateParams.username,
       message:$scope.user.message
     };
+    
     $http.post("https://messageapp1.herokuapp.com/messages", dataWithPost).then(function(response){
       console.log(response);
       $scope.data = response.data;
