164b747073494b990471fff8641d5cf2501fbe9d
Pramesh <pramesh_morang@hotmail.com>
2016-10-20 00:49:18 +0300

    client side file

---
+++ www/js/app.js
@@ -0,0 +1,46 @@
+
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+   .state('signin', {
+    url: '/signin',
+    templateUrl: 'templates/view1.html',
+      controller: 'SignCtrl'
+  })
+
+    .state('feed', {
+    url: '/feed',
+    templateUrl: 'templates/view2.html',
+      controller: 'FeedCtrl',
+      params :{
+      username:null
+  }
+  });
+    
+  $urlRouterProvider.otherwise('/signin');
+});
+  
---
+++ www/js/controllers.js
@@ -0,0 +1,40 @@
+angular.module('starter.controllers', [])
+
+.controller('SignCtrl', function($scope,$state) {
+
+   $scope.user = {
+       name: ""
+   } ;
+   $scope.signin= function()
+{ 
+       console.log($scope.user.name);
+       $state.go('feed',{username:$scope.user.name});
+}
+})
+
+.controller('FeedCtrl', function($scope,$http,$stateParams) {
+ $scope.user= {
+     message: ""
+ };
+    console.log($stateParams);
+    
+    $http.get("https://messageapp1.herokuapp.com/messages").then(function(response){
+        console.log(response);
+        $scope.data=response.data;
+});
+    
+$scope.send = function()
+  {
+    console.log($scope.user.message);
+    var dataWithPost={
+      username:$stateParams.username,
+      message:$scope.user.message
+    };
+    $http.post("https://messageapp1.herokuapp.com/messages", dataWithPost).then(function(response){
+      console.log(response);
+      $scope.data = response.data;
+    });
+
+  }
+})
+
